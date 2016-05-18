007ac52e1d1c8fafdeafa38ab3da834db819a1f5
nicooooo <nicooooo@outlook.fr>
2016-05-18 05:14:25 -0400

    exo1

---
+++ exo1.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Titre</title>
+    </head>
+
+    <body>
+    <div>
+		
+      <h2>
+        ajouter un artiste
+      </h2>
+      <p>
+        
+      </p>
+      
+			<form action="exo1.php" method="get">
+			<p>
+        <label>Nom</label> : <input type="text" name="nom" /> </p>
+      <p>
+        </p> <label>Prenom</label> : <input type="text" name="prenom" /></p>
+      <p>
+        <label>Annee de naissance</label> : <input type="int" name="annee" /></p>
+
+			<input type="submit" value="chercher" />
+				 </select>    
+   
+ </form>
+			
+      </div>
+
+  <aside>
+  
+    <h2>
+      Listes des Artistes
+    </h2>
+    
+    <p>
+      ----------------------------------------
+    </p>
+
+     <table border=1>
+          	<thead>
+             <tr>
+              <th>artiste</th>
+              <th>annee de naissance</th>
+             </tr>
+            <thead>
+    <?php
+
+							 $link=mysqli_connect("dwarves.iut-fbleau.fr","carlu","ludo1811","carlu"); 
+         $result = mysqli_query($link,"SELECT nom , prenom , anneeNaiss From Artiste ");
+           if ($result){
+             while($artiste=mysqli_fetch_assoc($result)){
+              echo "<tr>";
+              echo "<td>".$artiste['nom']."</td>";
+              echo"<td>".$artiste['anneeNaiss']."</td>";
+              echo "</tr>";
+			       } 
+           }   
+           else
+            die("<p>Erreur dans l'éxécution de la requête.</p>");
+         mysqli_close($link);
+        ?>
+							
+							
+							
+			   </table>				
+					 </aside>		
+						  </body>	
+		
+							
+
+</html>
